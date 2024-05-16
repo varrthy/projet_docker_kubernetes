22aaec007c8a9704e9a6b2c708d7741493baea03
varrthy <arrthyvettivelkumaran@gmail.com>
2024-05-16 23:33:59 +0200

    Add files via upload

---
+++ src/myWebApp.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, "myWebApp.html"));
+});
+
+app.listen(3000, function (){
+    console.log("app listening on port 3000!");
+});
+
+
+
